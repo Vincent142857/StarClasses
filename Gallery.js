e8653637738c5c18b0a113fe38d21bc2e7402420
Hunter1412 <111443913+Hunter1412@users.noreply.github.com>
2022-10-24 15:28:07 +0700

    Update Gallery.js

--- Gallery.js
+++ Gallery.js
@@ -808,7 +808,7 @@ $(".menu-sub").click(function (e) {
   sessionStorage.setItem("menuData", JSON.stringify(filterSubject(sub)));
   var linkData = JSON.parse(sessionStorage.getItem("menuData"));
 
-  if (window.open("/GalleryCourse.html", "_self") == null) {
+  if (window.open("/GalleryCourse.html", "_self")) {
     window.open("../GalleryCourse.html", "_self");
   } else {
     window.open("/GalleryCourse.html", "_self");
