1496b4d58a57898242b80866f760670e840515bf
Hunter1412 <111443913+Hunter1412@users.noreply.github.com>
2022-10-28 09:05:31 +0700

    add check user

--- cartForCourse.js
+++ cartForCourse.js
@@ -1,4 +1,4 @@
-// Shopping Cart API
+// A. Shopping Cart API
 // ************************************************
 
 var shoppingCart = (function () {
@@ -59,20 +59,6 @@ var shoppingCart = (function () {
     }
     saveCart();
   }
-
-  // Remove item from cart
-  // obj.removeItemFromCart = function (id) {
-  //   for (var i in cart) {
-  //     if (cart[i].id === id) {
-  //       cart[i].count--;
-  //       if (cart[i].count === 0) {
-  //         cart.splice(i, 1);
-  //       }
-  //       break;
-  //     }
-  //   }
-  //   saveCart();
-  // }
 
   // Remove all items from cart
   obj.removeItemFromCartAll = function (id) {
@@ -232,3 +218,298 @@ $('.show-cart').on("change", ".item-count", function (event) {
 });
 
 displayCart();
+
+
+
+//*********************************** */
+// B. Sign in/Sign up API
+// ************************************************
+
+var dataUser = (function () {
+  
+  user = [];
+  userCurrent =[];
+
+
+  // Constructor
+  function Item(email, phone, name, pwd) {
+    this.email = email;
+    this.phone = phone;
+    this.name = name;
+    this.pwd = pwd;
+    
+  }
+
+  // Save user information
+  function saveUser() {
+    localStorage.setItem('dataUser', JSON.stringify(user));
+  }
+  // Load user information
+  function loadUser() {
+    user = JSON.parse(localStorage.getItem('dataUser'));
+  }
+  if (localStorage.getItem("dataUser") != null) {
+    loadUser();
+  }
+
+  //save userCurrent
+  function saveUserCurrent(u) {
+    sessionStorage.setItem("infoUserCurrent", JSON.stringify(u));
+  }
+
+  //load userCurrent
+  function loadUserCurrent(){
+    userCurrent = JSON.parse(sessionStorage.getItem("infoUserCurrent"));
+  }
+  if (sessionStorage.getItem("infoUserCurrent") != null) {
+    loadUserCurrent();
+  }
+
+  var object = {};
+
+
+  // Add to dataUser, (localStorage)
+  object.addItemToUser = function (email, phone, name, pwd) {
+    for (var i in user) {
+      if (user[i].email === email) {
+        displaySignUpError(email);
+        return;
+      }
+    }
+    var item = new Item(email, phone, name, pwd);
+    user.push(item);
+    saveUser();
+    displaySignUpSuccess(name);
+  }
+
+  
+  // Check information of user-current
+  object.checkSignIn = function (email, pwd) {
+
+    let emailUser = user.filter(i => i.email == email) 
+    if (emailUser.length == 0) {
+      displaySignInErrorEmail();
+      return;
+    }
+
+    let signIn = false;
+    for (var i in user) {
+      if (user[i].email === email && user[i].pwd === pwd) {
+        saveUserCurrent(user[i]);
+        signIn = true;
+      }
+    }
+    if (signIn === true) {
+      displaySignInSuccess();
+    
+    }else {
+      displaySignInError()
+    }
+  }
+  // console.log(userCurrent.email);
+  // console.log(userCurrent.name);
+
+  return object;
+})();
+
+// *****************************************
+// Triggers / Events
+// ***************************************** 
+
+// Add item
+$(".add-to-user").click(function (e) {
+  e.preventDefault();
+  var email = $("#Email").val();
+  var phone = $("#Phone").val();
+  var name = $("#FullName").val();
+  var pwd = $("#Password").val();
+  
+  dataUser.addItemToUser(email, phone, name, pwd);
+  // displayUser();
+});
+
+
+// Check your account;
+$('.check-user').click(function (e) {
+  e.preventDefault();
+
+  var emailCurrent = $("#EmailCurrent").val();
+  var pwdCurrent = $("#PwdCurrent").val();
+
+  dataUser.checkSignIn(emailCurrent, pwdCurrent);
+  // displayUser();
+});
+
+
+if (sessionStorage.getItem("infoUserCurrent") != null){
+  displayUser();
+}
+
+function btnCheckOut() {
+  return (sessionStorage.getItem("infoUserCurrent") != null) ? true : false;
+}
+// console.log(sessionStorage.getItem("infoUserCurrent"));
+
+// display button userCurrent name in navbar-menu
+function displayUser() {
+  
+  var userA = userCurrent;
+  console.log(userA);
+
+  let string = userA.name.indexOf(" ") === -1 ? userA.name : userA.name.slice(0, userA.name.indexOf(" "));
+
+  let btnSignIn =
+    `<div class="nav-link dropdown col-12 mx-auto my-auto" style="right:1">
+      <a class="btn btn-light dropdown-toggle font-weight-bold" href="#" role="button" id="dropdownMenuLink"
+        data-mdb-toggle="dropdown" aria-expanded="false">
+        ${string}
+      </a>
+      <div class="dropdown-menu mt-0" aria-labelledby="dropdownMenuLink"
+      style="border-top-left-radius: 0;
+            border-top-right-radius: 40px;">
+
+        <div class="list-group list-group-flush">
+          <a class="list-group-item list-group-item-action" href="#">
+            <i class="fas fa-user me-2"></i>My Account
+          </a>
+        
+          <a class="list-group-item log-out" href="#" type="button">
+            <i class="fas fa-sign-out-alt"></i>
+            Log out
+          </a>
+        </div>
+
+      </div>
+    </div>`;
+
+  $(".user-name").html(btnSignIn);
+
+  
+}
+//*** Logout ***
+$(".log-out").click(function (e) {
+  e.preventDefault();
+  
+  sessionStorage.removeItem("infoUserCurrent");
+  location.reload();
+});
+
+// displayUser();
+
+// *** display messenger in Sign in page ***
+function displaySignInSuccess(){
+  let string = 
+    `<div class="modal-body text-start text-black p-0">
+
+      <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png"
+          width="50%" alt="Star Classes">
+      </p>
+
+      <h4 class="mb-3 text-warning text-center">You are successfully logged in</h4>
+
+      <h2 class="mb-1 text-warning text-center">Welcome to Star Classes </h2>
+      
+    </div>
+    
+    <div class="modal-footer d-flex justify-content-center border-top-0 py-4">
+      <a href="./index.html" type="button" class="btn btn-warning btn-lg mb-1">
+        Let's enjoy our courses now!
+      </a>
+    </div>`;
+
+    $(".str-sign-in").html(string);
+    // $('a.signIn').attr('href','./index.html');
+}
+
+function displaySignInErrorEmail() {
+
+  let messenger = 
+    `<div class="modal-body text-start text-black p-0">
+      <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png"
+          width="50%" alt="Star Classes">
+      </p>
+
+      <h4 class="mb-1 text-danger text-center">
+        <i class="fas fa-exclamation-triangle fa-2x"></i>
+        There was a problem
+      </h4>
+      <h5 class="mb-5 text-center">We cannot find an account with that email address!</h5>
+      
+    </div>
+    <div class="modal-footer d-flex justify-content-center border-top-0 py-3">
+      <a href="./SignUp.html" type="button" class="btn btn-warning btn-lg mb-1">
+        Create account
+      </a>
+    </div>`;
+
+  $(".str-sign-in").html(messenger);
+
+}
+
+function displaySignInError(){
+  let messenger = 
+    `<div class="modal-body text-start text-black p-0">
+      <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png"
+          width="50%" alt="Star Classes">
+      </p>
+
+      <h4 class="mb-5 text-danger text-center">
+        <i class="fas fa-exclamation-triangle fa-2x"></i>
+        Your password is incorrect!
+      </h4>
+      
+    </div>`;
+
+  $(".str-sign-in").html(messenger);
+}
+
+//*** Display message  in Sign-up page  */
+function displaySignUpSuccess(name) {
+  
+  let message =
+  `<div class="modal-body text-start text-black p-0">
+    <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png" width="50%" alt="Star Classes"></p>
+
+    <h4 class="mb-3 text-warning text-center">Your account has been created</h4>
+  
+    <h2 class="mb-1 text-warning text-center">Welcome ${name} to Star Classes </h2>
+    
+  </div>
+
+  <div class="modal-footer d-flex justify-content-center border-top-0 py-3">
+    <a href="./SignIn.html" type="button" class="btn btn-warning btn-lg mb-1">
+      Sign in to enjoy our courses now!
+    </a>
+
+  </div>`
+
+  $(".str-sign-up").html(message);
+}
+
+function displaySignUpError(email) {
+  let message =
+  `<div class="modal-body text-start text-black p-0">
+
+    <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png" width="50%" alt="Star Classes"></p>
+
+    <div class="mb-5 p-3">
+      <h4 class="text-danger">
+        <i class="fas fa-exclamation-triangle fa-2x"></i>
+        Email address already in use
+      <h4>
+      <p>You indicated you're a new customer, but an account already exists with the email address <u>${email}</u>.</p>
+    </div>
+    
+  </div>
+
+  <div class="modal-footer d-flex justify-content-center border-top-0 py-4">
+    <a href="./SignIn.html" type="button" class="btn btn-warning btn-lg mb-1">
+      Sign in now!
+    </a>
+
+  </div>` 
+
+  $(".str-sign-up").html(message);
+}
+
+
--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-// Shopping Cart API
+// Sign in/Sign up API
 // ************************************************
 
 var dataUser = (function () {
@@ -18,15 +18,12 @@ var dataUser = (function () {
 
   // Save user information
   function saveUser() {
-    // sessionStorage.setItem('dataUser', JSON.stringify(user));
     localStorage.setItem('dataUser', JSON.stringify(user));
   }
   // Load user information
   function loadUser() {
-    // user = JSON.parse(sessionStorage.getItem('dataUser'));
     user = JSON.parse(localStorage.getItem('dataUser'));
   }
-  // if (sessionStorage.getItem("dataUser") != null) {
   if (localStorage.getItem("dataUser") != null) {
     loadUser();
   }
@@ -44,24 +41,25 @@ var dataUser = (function () {
     loadUserCurrent();
   }
 
-  var obj = {};
+  var object = {};
 
-  // Add to dataUser
-  obj.addItemToUser = function (email, phone, name, pwd) {
+
+  // Add to dataUser, (localStorage)
+  object.addItemToUser = function (email, phone, name, pwd) {
     for (var i in user) {
       if (user[i].email === email) {
-        alert("Your email had account! Please sign in with your account.")
+        displaySignUpError(email);
         return;
       }
     }
     var item = new Item(email, phone, name, pwd);
-    user.push(item); //save to array user
-    saveUser(); //save to json dataUser
-
+    user.push(item);
+    saveUser();
+    displaySignUpSuccess(name);
   }
 
   // Remove item from cart
-  obj.removeItemFromUser = function (email) {
+  object.removeItemFromUser = function (email) {
     for (var i in user) {
       if (user[i].email === email) {
         user.splice(i, 1);
@@ -70,26 +68,35 @@ var dataUser = (function () {
     }
     saveUser();
   }
+  
 
   // Check information of user-current
-  obj.checkSignIn = function (email, pwd) {
+  object.checkSignIn = function (email, pwd) {
+
+    let emailUser = user.filter(i => i.email == email) 
+    if (emailUser.length == 0) {
+      displaySignInErrorEmail(email);
+      return;
+    }
+
+    let signIn = false;
     for (var i in user) {
       if (user[i].email === email && user[i].pwd === pwd) {
         saveUserCurrent(user[i]);
-        return true;
+        signIn = true;
       }
     }
-    return false;
+    if (signIn === true) {
+      displaySignInSuccess();
+    
+    }else {
+      displaySignInError()
+    }
   }
-  console.log(userCurrent.email);
-  console.log(userCurrent.name);
+  // console.log(userCurrent.email);
+  // console.log(userCurrent.name);
 
-  // List cart
-  obj.listUser = function () {
-    return userCurrent;
-  }
-
-  return obj;
+  return object;
 })();
 
 // *****************************************
@@ -105,38 +112,182 @@ $(".add-to-user").click(function (e) {
   var pwd = $("#Password").val();
   
   dataUser.addItemToUser(email, phone, name, pwd);
-  displayUser();
+  // displayUser();
 });
 
 
 // Check your account;
-$('.check-user').click(function () {
+$('.check-user').click(function (e) {
+  e.preventDefault();
+
   var emailCurrent = $("#EmailCurrent").val();
   var pwdCurrent = $("#PwdCurrent").val();
 
-  var signIn = dataUser.checkSignIn(emailCurrent, pwdCurrent);
-  if (signIn === true) {
-    alert("Welcome to Star Classes!");
-    displayUser();
-  }else {
-    alert("Your email or your password is failed!");
-  }
+  dataUser.checkSignIn(emailCurrent, pwdCurrent);
+  // displayUser();
 });
 
 
+
+if (sessionStorage.getItem("infoUserCurrent") != null){
+  displayUser();
+}
+// console.log(sessionStorage.getItem("infoUserCurrent"));
+
+
 function displayUser() {
-  var userArray = dataUser.listUser();
-  console.log(userArray);
+  // var userA = dataUser.listUser();
+  var userA = userCurrent;
+  console.log(userA);
 
-  var output = `<a class="nav-link text-center active rounded-circle btn btn-warning" href="#">
-      <span class="user-name"></span>
-    </a>`;
+  let string = userA.name.indexOf(" ") === -1 ? userA.name : userA.name.slice(0, userA.name.indexOf(" "));
 
-    $(".user-name").html(userCurrent.name);
+  let btnSignIn =
+    `<div class="nav-link dropdown col-12 mx-auto my-auto" style="right:1">
+      <a class="btn btn-light dropdown-toggle font-weight-bold" href="#" role="button" id="dropdownMenuLink"
+        data-mdb-toggle="dropdown" aria-expanded="false">
+        ${string}
+      </a>
+      <div class="dropdown-menu mt-0" aria-labelledby="dropdownMenuLink"
+      style="border-top-left-radius: 0;
+            border-top-right-radius: 40px;">
+
+        <div class="list-group list-group-flush">
+          <a class="list-group-item list-group-item-action" href="#">
+            <i class="fas fa-user me-2"></i>My Account
+          </a>
+        
+          <a class="list-group-item log-out" href="#" type="button">
+            <i class="fas fa-sign-out-alt"></i>
+            Log out
+          </a>
+        </div>
+
+      </div>
+    </div>`;
+
+  $(".user-name").html(btnSignIn);
   
-  // $('.show-user').html(output);
   
 }
+//*** Logout ***
+$(".log-out").click(function (e) {
+  e.preventDefault();
+  
+  sessionStorage.removeItem("infoUserCurrent");
+  location.reload();
+});
 
-displayUser();
+// displayUser();
+
+// *** display messenger in Sign in page ***
+function displaySignInSuccess(){
+  let string = 
+    `<div class="modal-body text-start text-black p-0">
+
+      <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png"
+          width="50%" alt="Star Classes">
+      </p>
+
+      <h4 class="mb-3 text-warning text-center">You are successfully logged in</h4>
+
+      <h2 class="mb-1 text-warning text-center">Welcome to Star Classes </h2>
+      
+    </div>
+    
+    <div class="modal-footer d-flex justify-content-center border-top-0 py-4">
+      <a href="./index.html" type="button" class="btn btn-warning btn-lg mb-1 clear-cart signIn">
+        Let's enjoy our courses now!
+      </a>
+    </div>`;
+
+    $(".str-sign-in").html(string);
+    // $('a.signIn').attr('href','./index.html');
+}
+
+function displaySignInErrorEmail(email) {
+
+  let messenger = 
+    `<div class="modal-body text-start text-black p-0">
+      <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png"
+          width="50%" alt="Star Classes">
+      </p>
+
+      <h4 class="mb-1 text-danger text-center">
+        <i class="fas fa-exclamation-triangle fa-2x"></i>
+        There was a problem
+      </h4>
+      <h5 class="mb-5 text-center">We cannot find an account with that email address!</h5>
+      
+    </div>`;
+
+  $(".str-sign-in").html(messenger);
+
+}
+
+function displaySignInError(){
+  let messenger = 
+    `<div class="modal-body text-start text-black p-0">
+      <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png"
+          width="50%" alt="Star Classes">
+      </p>
+
+      <h4 class="mb-5 text-danger text-center">
+        <i class="fas fa-exclamation-triangle fa-2x"></i>
+        Your password is incorrect!
+      </h4>
+      
+    </div>`;
+
+  $(".str-sign-in").html(messenger);
+}
+
+//*** Display message  in Sign-up page  */
+function displaySignUpSuccess(name) {
+  
+  let message =
+  `<div class="modal-body text-start text-black p-0">
+    <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png" width="50%" alt="Star Classes"></p>
+
+    <h4 class="mb-3 text-warning text-center">Your account has been created</h4>
+  
+    <h2 class="mb-1 text-warning text-center">Welcome ${name} to Star Classes </h2>
+    
+  </div>
+
+  <div class="modal-footer d-flex justify-content-center border-top-0 py-4">
+    <a href="./SignIn.html" type="button" class="btn btn-warning btn-lg mb-1 clear-cart">
+      Sign in to enjoy our courses now!
+    </a>
+
+  </div>`
+
+  $(".str-sign-up").html(message);
+}
+
+function displaySignUpError(email) {
+  let message =
+  `<div class="modal-body text-start text-black p-0">
+
+    <p class="modal-title mb-5 bg-dark p-2" id="ModalLabel"><img src="./img/star-classes-4.png" width="50%" alt="Star Classes"></p>
+
+    <div class="mb-5 p-4">
+      <h4 class="text-danger">
+        <i class="fas fa-exclamation-triangle fa-2x"></i>
+        Email address already in use
+      <h4>
+      <p>You indicated you're a new customer, but an account already exists with the email address <u>${email}</u>.</p>
+    </div>
+    
+  </div>
+
+  <div class="modal-footer d-flex justify-content-center border-top-0 py-4">
+    <a href="./SignIn.html" type="button" class="btn btn-warning btn-lg mb-1 clear-cart">
+      Sign in now!
+    </a>
+
+  </div>` 
+
+  $(".str-sign-up").html(message);
+}
 
